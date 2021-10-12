0301fbc0002ff2f1a9efb3d8800a2fc3c5c5dde1
Bernabé Diharce <bdiharce@claro.com.ar>
2021-10-12 11:10:21 -0300

    subo app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker!! by Berna"
+    return "Flask inside Docker!! Updated using GIT"
 
 
 if __name__ == "__main__":
